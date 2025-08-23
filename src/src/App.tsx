1c3707387179928fc18af423bebb187e529cee3b
Szymon <szymon.osinski.2009@gmail.com>
2025-08-23 11:41:10 +0200

    setup routes

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,11 +1,26 @@
 import "./App.css";
-import { Navbar } from "./components/Navbar";
+// import { Navbar } from "./components/Navbar";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Navbar />
-    </>
+    // <>
+    //   <Navbar />
+    // </>
+    <div className="p-4">
+      <nav className="mb-4 flex gap-4">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<>fds</>} />
+        <Route path="/about" element={<>dfsd</>} />
+        <Route path="/contact" element={<>fdsfsd</>} />
+        <Route path="*" element={<h1>404 - Not Found</h1>} />
+      </Routes>
+    </div>
   );
 }
 
--- src/src/main.tsx
+++ src/src/main.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { ThemeProvider } from "@/components/theme-provider.tsx";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <HashRouter>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </HashRouter>
   </React.StrictMode>
 );
