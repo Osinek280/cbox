f56be79e195cb9de627d7cec2316c4f655f8d5d3
Szymon <szymon.osinski.2009@gmail.com>
2025-08-26 22:17:04 +0200

    fix: refresh token

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,14 +1,12 @@
 "use client";
 
-import { useAuth } from "@/context/AuthContext";
+import { filesApi } from "@/api/files";
 import { useEffect } from "react";
 
 export default function FilesPage() {
-  const { refreshAccessToken } = useAuth();
-
   useEffect(() => {
-    refreshAccessToken();
-  }, [refreshAccessToken]);
+    filesApi();
+  }, []);
   return (
     <>
       <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
